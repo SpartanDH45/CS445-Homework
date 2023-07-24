af1cd2591a87ec5194acea78bbb17d506fc1d505
SpartanDH45 <donaldjhammer@gmail.com>
2023-07-24 12:26:09 -0600

    Update DandD.cpp

--- CS445-Summer/ASN_6/DandD.cpp
+++ CS445-Summer/ASN_6/DandD.cpp
@@ -44,12 +44,13 @@ void printMap(char *map){
         printf("X\n");
     }
 }
-
+int zombieDefault[] = (22, 8, 4, 2, 13, 6, 16, 3, 6, 5, 106, 1, 0, 0); 
 int charMonHP[100], charMonAC[100], charMonSpd[100], charMonProf[100];
 int charMonStr[100], charMonDex[100], charMonCon[100];
 int charMonInt[100], charMonWis[100], charMonCha[100];
 int charMonAttackMDam[100], charMonAttackRDam[100];
 int charMonAttackMRan[100], charMonAttackRRan[100];
+char charMonCharMarker[100];
 //XY Positions given as a single integer XXYY
 int charMonXYPos[100];
 //
@@ -57,6 +58,7 @@ char mapDisplay[500];
 char mapBackground[500] = "_x_x_~__xxxx__~_xx___________xx________x___x____________x________~xxx__xxx~_____~~__~~~~~__~~~~~__~~_____~________~______xx________________x___________________x_xx__~____x___~____________x_xx_x______x_______x_xx________x___x_~___xx_x_~_____x______x_xx________xx__x______x________x_____~________~_____~~__~~~~~__~~~~~__~~___x_~________~_______________________x__x________________x____x_~________~_____";
 char pathingMap[500];
 
+void setCharMon(int idNum, int HP, int AC, int Spd, int Prof, int )
 
 int main(){
     srand((unsigned) time(NULL));
