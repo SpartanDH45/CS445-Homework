570fde602d601f0dc100fb4293874693aeb1c7e8
SpartanDH45 <90274353+SpartanDH45@users.noreply.github.com>
2023-07-24 21:20:48 -0600

    Update DandD.cpp

--- CS445-Summer/ASN_6/DandD.cpp
+++ CS445-Summer/ASN_6/DandD.cpp
@@ -65,20 +65,42 @@ void printMap(char *map){
         }
     }
 }
-int zombieDefault[] = (22, 8, 4, 2, 13, 6, 16, 3, 6, 5, 106, 1, 0, 0); 
+
+void printIntMap(int *map){
+    for(int i = 0; i < mapHeight; i++){
+        for(int j = 0; j < mapWidth; j++){
+            if(map[(i * mapWidth) + j] < 10){
+                printf("0%d", map[(i * mapWidth) + j]);
+            } else {
+                printf("%d", map[(i * mapWidth) + j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+//Order for storage: HP,AC,Spd,Prof,Str,Dex,Con,Int,Wis,Cha,
+//                   MDam,MRan,Mabl,RDam,RRan,RAbl,RSav
+int zombieDefault[] = {22, 8, 4, 2, 1, -2, 3, 3, 0, -3, 106, 1, 1,  0, 0, 1, 0};
+int dhulgenStats[] = {50, 18, 5, 3, 4, 0, 3, 1, 1, -1, 206, 1, 1, 106, 6, 1, 0};
+int ogrimStats[] = {50, 18, 5, 3, 2, 2, 3, 1, 4, -1, 108, 1, 1, 208, 12, 5, 2};
+int kolgarStats[] = {50, 16, 5, 3, 1, 2, 3, 4, 1, -1, 108, 1, 1, 2010, 20, 4, 0};
+int torbinStats[] = {50, 16, 5, 3, 4, 2, 2, 1, 0, 3, 108, 1, 1, 104, 6, 1, 0};
 int charMonHP[100], charMonAC[100], charMonSpd[100], charMonProf[100];
 int charMonStr[100], charMonDex[100], charMonCon[100];
 int charMonInt[100], charMonWis[100], charMonCha[100];
-int charMonAttackMDam[100], charMonAttackRDam[100];
-int charMonAttackMRan[100], charMonAttackRRan[100];
+int charMonAttackMDam[100], charMonAttackMRan[100], charMonAttackMAbl[100];
+int charMonAttackRDam[100], charMonAttackRRan[100], charMonAttackRAbl[100];
+int charMonAttackRSav[100];
 char charMonMapMarker[100];
 //XY Positions given as a single integer XXYY
 int charMonXYPos[100];
 //
-char mapDisplay[500]  = "_x_x_~__xxxx__~_xx___________xx________x___x____________x________~xxx__xxx~_____~~__~~~~~__~~~~~__~~_____~________~______xx________________x___________________x_xx__~____x___~____________x_xx_x______x_______x_xx________x___x_~___xx_x_~_____x______x_xx________xx__x______x________x_____~________~_____~~__~~~~~__~~~~~__~~___x_~________~_______________________x__x________________x____x_~________~_____";
+char mapDisplay[500];
 char mapBackground[500] = "_x_x_~__xxxx__~_xx___________xx________x___x____________x________~xxx__xxx~_____~~__~~~~~__~~~~~__~~_____~________~______xx________________x___________________x_xx__~____x___~____________x_xx_x______x_______x_xx________x___x_~___xx_x_~_____x______x_xx________xx__x______x________x_____~________~_____~~__~~~~~__~~~~~__~~___x_~________~_______________________x__x________________x____x_~________~_____";
 int idMap[500];
-int pathingMap[500] = (-1);
+int pathingMap[500] = {-1};
+int entityCount = 0;
 
 void setCharMon(int idNum, int *stats, char marker, int x, int y){
     charMonHP[idNum] = stats[0];
@@ -93,8 +115,11 @@ void setCharMon(int idNum, int *stats, char marker, int x, int y){
     charMonCha[idNum] = stats[9];
     charMonAttackMDam[idNum] = stats[10];
     charMonAttackMRan[idNum] = stats[11];
-    charMonAttackRDam[idNum] = stats[12];
-    charMonAttackRRan[idNum] = stats[13];
+    charMonAttackMAbl[idNum] = stats[12];
+    charMonAttackRDam[idNum] = stats[13];
+    charMonAttackRRan[idNum] = stats[14];
+    charMonAttackRAbl[idNum] = stats[15];
+    charMonAttackRSav[idNum] = stats[16];
     charMonMapMarker[idNum] = marker;
     charMonXYPos[idNum] = (x*100)+y;
     if(x > 19 || x < 0 || y > 19 || y < 0){
@@ -103,18 +128,37 @@ void setCharMon(int idNum, int *stats, char marker, int x, int y){
         idMap[(y * mapWidth) + x] = idNum;
         mapDisplay[(y * mapWidth) + x] = marker;
     }
+    entityCount++;
 
 
 }
-void getLowestNeighbor(int x, int y){
-    
+int getLowestNeighbor(int x, int y){
+    int lowest = 1000;
+    for(int i = -1; i < 2; i++){
+        for(int j = -1; j < 2; j++){
+            if(i != 0 && j != 0){
+                if(pathingMap[((y + i) * mapWidth) + x + j]<lowest){
+                    lowest = pathingMap[((y + i) * mapWidth) + x + j];
+                }
+            }
+        }
+    }
+    return lowest;
+}
+
+bool isOpenSpot(int x, int y){
+    if(idMap[(y * mapWidth) + x] != -2){
+        return false;
+    } else {
+        return true;
+    }
 }
 
 void setPathingMap(){
     if(pathingMap[0] == -1){
         for(int i = 0; i < mapHeight; i++){
             for(int j = 0; j < mapWidth; j++){
-                pathingMap[(i*mapWidth) + j] = 100;
+                pathingMap[(i*mapWidth) + j] = 99;
             }
         }
     }
@@ -135,7 +179,61 @@ void setPathingMap(){
     }
 }
 
+void setIDMap(){
+    for(int i = 0; i < mapHeight; i++){
+        for(int j = 0; j < mapWidth; j++){
+            if(mapBackground[(i * mapWidth) + j] == '_'){
+                idMap[(i * mapWidth) + j] = -2;
+            } else {
+                idMap[(i * mapWidth) + j] = -1;
+            }
+        }
+    }
+    for(int i = 0; i < entityCount; i++){
+        int x = charMonXYPos[i] / 100;
+        int y = charMonXYPos[i] - (x * 100);
+        idMap[(y * mapWidth) + x] = i;
+    }
+}
+
+void setMapDisplay(){
+    for(int i = 0; i < mapHeight; i++){
+        for(int j = 0; j < mapWidth; j++){
+            mapDisplay[(i * mapWidth) + j] = mapBackground[(i * mapWidth) + j];
+        }
+    }
+    for(int i = 0; i < entityCount; i++){
+        int x = charMonXYPos[i] / 100;
+        int y = charMonXYPos[i] - (x * 100);
+        mapDisplay[(y * mapWidth) + x] = charMonMapMarker[i];
+    }
+}
+
+void spawnMob(int idNum, int *stats, char marker){
+    int x = roll(20)-1;
+    int y = roll(20)-1;
+    //while (x,y) corresponds to an invalid spawning space (occupied or too close)
+    while(mapBackground[(y * mapWidth) + x] != '_' || (y > 15 && (x > 5 && x < 14))){
+        x = roll(20)-1;
+        x = roll(20)-1;
+    }
+    setCharMon(idNum, stats, marker, x, y);
+}
+
 int main(){
     srand((unsigned) time(NULL));
+    setCharMon(0, dhulgenStats, 'D', 9, 18);
+    setCharMon(1, ogrimStats, 'O', 10, 18);
+    setCharMon(2, kolgarStats, 'K', 9, 19);
+    setCharMon(3, torbinStats, 'T', 10, 19);
+    for(int i = 4; i < 24; i++){
+        spawnMob(i, zombieDefault, 'Z');
+    }
+    setIDMap();
+    setMapDisplay();
     printMap(mapBackground);
+    printf("\n");
+    printMap(mapDisplay);
+    printf("\n");
+
 }
